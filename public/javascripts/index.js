792801a2b5f587b038ef4169a2958b6a4eabbebf
josuecorea5 <josuepaiz487@gmail.com>
2022-03-29 12:42:46 -0600

    Add button for copy url

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,20 @@
 const copyUrl = document.querySelectorAll('.copy-url');
 
 copyUrl.forEach(item => {
-  item.addEventListener('click', () => {
-    console.log('Hello');
+  item.addEventListener('click', (e) => {
+    if(e.target.classList.contains('copy-url')) {
+      const id = e.target.getAttribute("data-id");
+      navigator.clipboard.writeText(`http://localhost:3000/${id}`)
+        .then(() => {
+          e.target.classList.remove('bg-secondary');
+          e.target.classList.add('bg-success')
+            e.target.textContent = "Copied!";
+            setTimeout(() => {
+              e.target.textContent = "Copy";
+              e.target.classList.remove('bg-success');
+              e.target.classList.add('bg-secondary')
+            },3000)
+        });
+    }
   })
 })
